1cadaec298877576fbe37897290373ca5cdef9d7
karla16Santoyo99 <64182574+karla16Santoyo99@users.noreply.github.com>
2022-02-12 18:13:53 -0600

    Icono

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -62,9 +62,10 @@ function Home() {
                 </Button>
             </Grid>
             <Grid item xs={4}>
+                <a href='https://github.com/karla16Santoyo99/Spotify1'>
                 <Button variant="contained" >
             Github
-                </Button>
+                </Button></a>
             </Grid>
            
             <Grid
